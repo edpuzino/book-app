dd23d77e5390eb8994ef657bae60a8c59316376b
edpuzino <edpuzino@yahoo.com>
2018-08-27 19:43:20 -0700

    some styling

--- server.js
+++ server.js
@@ -6,36 +6,33 @@ const express = require('express');
 const pg = require('pg');
 
 const app = express();
-const PORT = process.env.PORT;
-app.set('view engine', 'ejs');
+
 const client = new pg.Client(process.env.DATABASE_URL);
+const PORT = process.env.PORT;
 client.connect();
 client.on('error', err => console.error(err));
 
-
+app.set('view engine', 'ejs');
 app.use(express.static('./public'));
+
 
 app.get('/ping', (request, response) => {
   response.send('pong');
 });
 
 app.get('/hello', (request, response) => {
-  response.render('index.ejs');
+  response.render('index', {books: []});
 });
 
-app.get('/books', getBooks);
+app.get('/books', (request, response) => {
+  client.query('SELECT author, title, image_url FROM books;')
+    .then(results => {
+      response.render('index', { books: results.rows });
+    });
+});
 
 app.get('*', (request, response) => {
   response.render('pages/error');
 });
 
-function getBooks(request, response) {
-  client.query('SELECT title, author, image_url FROM books;')
-    .then(results => {
-      console.log(results.rows);
-      response.render('index', { books: results.rows });
-    });
-}
-
 app.listen(PORT, () => console.log('Listening on PORT', PORT));
-
